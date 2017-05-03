e5ae9e4d48b4076c67738100d26e273c3f1c2005
qicuili <qicuili@qicuilideMacBook-Pro.local>
2017-05-03 17:53:31 +0800

    上传
    上传

--- BaseFrameworksDemo/ViewController.swift
+++ BaseFrameworksDemo/ViewController.swift
@@ -7,11 +7,41 @@
 //
 
 import UIKit
-
+import Moya
+import RxSwift
 class ViewController: UIViewController {
-
+    lazy var disposeBag:DisposeBag = {
+        return DisposeBag()
+    }()
+    var model : GetVillageListModel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Networking.newDefaultNetworking()
+            .request(API.getvillageList(keyword:"",count:"10",page:"\(1)"))
+            .filterSuccessfulStatusCodes()
+            .mapJSON()
+            .mapToIgnoreOutside(object: GetVillageListModel.self)
+            .subscribe{[weak self] even in
+                //                self?.myTableView!.endRefresh()
+                switch even {
+                case .next(let value):
+                    self?.model = value
+                    print("next---\(value)-----")
+                    //self?.myTableView?.reloadData()
+                    
+                case .completed:
+                    log.debug("")
+                    //self?.hidePageLoading()
+                case .error(let error):
+                    log.error(error)
+                    
+                }
+                
+                
+            }.addDisposableTo(disposeBag)
+        
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
